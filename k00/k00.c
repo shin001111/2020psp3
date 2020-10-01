02652a7dbe500aab539cbef88616323c44107710
shin001111 <zin72384@gmail.com>
2020-10-01 10:33:33 +0900

    changed

--- k00/k00.c
+++ k00/k00.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
-
 int main(void)
 {
-    printf("H World\n");
+int *p; // int型を指すポインタ
+int a = 100;
+printf("a=%d¥n", a);
+p = &a;
+*p = 10;
+printf("a=%d, *p=%d¥n", a, *p);
+printf("address of a = %x, value of p = %x¥n", &a, p);
 }
